0583989902c005f5081e22ac2480208b3c7a23bd
Zachary McNulty <zach.mcnulty1992@gmail.com>
2016-02-08 12:55:08 -0500

    added a working gui i will begin to refactor the code to be conform to the protocall we went over in class

---
+++ src/client/Main.java
@@ -0,0 +1,73 @@
+package client;
+
+import java.awt.BorderLayout;
+
+import javax.swing.*;
+
+public class Main {
+
+	//the frame
+	public static JFrame frame; //this is what all the component objects get added to 
+	
+	//Panels
+	public static JPanel buttonPanel; //where we add buttons
+	public static JPanel instPanel; //where the instructions go
+	public static JPanel BoardPanel; //where the board will be desplayed
+	public static JPanel inputPanel; //where the input will go
+	
+	//Buttons
+	public static JButton SinglePlayerButton; //button for single player menu
+	public static JButton multiplayerButton; //button for the multiplayer menu
+	
+	//labels
+	public static JLabel instLabel;
+	
+	public static void main(String[] args) {
+		//init the frame
+		frame = new JFrame();
+		
+		//init the panels we need for the main menu
+		buttonPanel = new JPanel();
+		instPanel = new JPanel();
+		BoardPanel = new JPanel();
+		inputPanel = new JPanel();
+		
+		//init the buttons
+		SinglePlayerButton = new JButton("Single Player");
+		multiplayerButton = new JButton("Multiplayer");
+		
+		//allternately we can set text to a button using:
+		//SinglePlayerButton.setText("text goes here")
+		
+		//init our label
+		instLabel = new JLabel("Here's some instructions");
+		
+		//now we will populate the panels
+		populatePanels();
+		
+		//now our panels for buttons and instructions are populated
+		
+		//now we add the panels to the frame in the arangment we want
+		frame.setLayout(new BorderLayout()); //we add a layout manager to the frame
+		frame.add(buttonPanel, BorderLayout.SOUTH); //add the button panel to the bottom of the GUI
+		frame.add(instPanel, BorderLayout.CENTER); //add the instructions to the frame 
+		
+		frame.setVisible(true); //this makes the GUI visible 
+		frame.pack(); // if we pack then our GUI components will be tightly packed together
+	}
+	
+	public static void populatePanels(){
+		
+		//buttons first
+		buttonPanel.setLayout(new BorderLayout()); //we set the layout
+		buttonPanel.add(SinglePlayerButton, BorderLayout.WEST); //add a button to the left side of the panel
+		buttonPanel.add(multiplayerButton, BorderLayout.EAST); //add a button to the right side of the panel
+		
+		//now we add the instructions to the instPanel
+		instPanel.add(instLabel);
+		
+		//thats all for the initial setup of the panels
+	}
+	
+	
+}

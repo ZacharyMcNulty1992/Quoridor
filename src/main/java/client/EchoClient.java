564ee75526e66778d3a89116be08bb3e69a06e0c
Michael Cataldo <cataldmj194@potsdam.edu>
2016-02-10 16:18:46 -0500

    Added basic EchoServer and EchoClient.

---
+++ src/main/java/client/EchoClient.java
@@ -0,0 +1,37 @@
+/* Name : Michael Cataldo
+   Class: CIS 410
+   Assignment: p002 - BirthdayServer/Client
+   Due  : 02/12/2016 */
+package client;
+
+import java.net.Socket;
+import java.util.*;
+import java.io.*;
+
+public class EchoClient {
+    public final static String DEFAULT_MACHINE_NAME = "localhost";
+    
+    public static void main(String[] args) throws Exception{
+	EchoClient bc = new EchoClient("localhost",3939);
+	bc.run();
+    }
+
+    private Socket server;
+    public EchoClient(String host, int port)throws Exception{
+	server = new Socket(host,port);
+    }
+
+    public void run() throws Exception{
+	Scanner sin = new Scanner(server.getInputStream());
+	PrintStream sout = new PrintStream(server.getOutputStream());
+	Scanner keyboard = new Scanner(System.in);
+	String line = keyboard.nextLine();
+	while(true){
+	    sout.println(line);
+	    String serverLine = sin.nextLine();
+	    System.out.println(serverLine);
+	    line = keyboard.nextLine();
+	}
+    }
+
+}

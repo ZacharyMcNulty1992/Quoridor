d00e1c47217518a1a39a7637d9f1cc97adaeaff0
Zachary McNulty <zach.mcnulty1992@gmail.com>
2016-02-13 15:08:18 -0500

    this is the class for the nodes of our graph that will represent the board

---
+++ src/main/java/client/Space.java
@@ -0,0 +1,30 @@
+package client;
+
+public class Space(){
+
+//variables can be accessed by calling them 
+//thus we do not need getters.
+public Space Left;
+public Space Right;
+public Space Up;
+public Space Down;
+
+//coords on the Board
+public int x;
+public int y;
+
+/*
+ * Space
+ * 
+ * Params: A pointer to each adj node, x coord, y coord
+ */
+public Space(Space up, Space down, Space left, Space right, int x, int y){	
+	this.x = x;
+	this.y = y;
+	this.Left = left;
+	this.Right = right;
+	this.Down = down;
+	this.Up = up;
+}//end of constructor
+
+}//end of class

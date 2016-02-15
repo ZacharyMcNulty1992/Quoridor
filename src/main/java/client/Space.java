4c3df0db7eb133234d0647a2f33654715477fdd2
Brandon Williams <williabk198@potsdam.edu>
2016-02-15 13:20:54 -0500

    Fix GameBoard clas so that test now pass. Changed documentation to Javadoc to Space class. The space class now extends Point from java.awt.

--- src/main/java/client/Space.java
+++ src/main/java/client/Space.java
@@ -1,34 +1,34 @@
 package main.java.client;
 
-public class Space {
+import java.awt.Point;
 
-	// variables can be accessed by calling them
-	// thus we do not need getters.
-	public Space Left;
-	public Space Right;
-	public Space Up;
-	public Space Down;
-	public boolean occupied;
-
-	// coords on the Board
-	public int x;
-	public int y;
-
-	/*
-	 * constructor
-	 * 
-	 * Params: only the x and y coords
-	 */
-	public Space(int x, int y){
-		this.x = x;
-		this.y = y;
-	}
-	
-	/*
-	 * setOccupied Params: boolean weather the space has a player on it or not
-	 */
-	public void setOccupied(boolean x) {
-		occupied = x;
-	}
-
+public class Space extends Point{
+    
+    // variables can be accessed by calling them
+    // thus we do not need getters.
+    public Space Left;
+    public Space Right;
+    public Space Up;
+    public Space Down;
+    public boolean occupied;
+    
+    /**
+     * constructor
+     * 
+     * @param x - the x-coordinate of the space
+     * @param y - the y-coordinate of the space
+     */
+    public Space(int x, int y) {
+	super(x,y);
+    }
+    
+    /**
+     * setOccupied
+     *
+     * @param x - boolean value of weather the space has a player on it or not
+     */
+    public void setOccupied(boolean x) {
+	occupied = x;
+    }
+    
 }// end of class

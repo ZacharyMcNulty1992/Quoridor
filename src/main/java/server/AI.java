9779c0833cb92c201c60fadddfdb0fe2787f6ad3
Zachary McNulty <zach.mcnulty1992@gmail.com>
2016-02-17 13:20:19 -0500

    the ai file was not added for some reason

---
+++ src/main/java/server/AI.java
@@ -0,0 +1,209 @@
+package main.java.server;
+
+import java.util.ArrayList;
+
+import main.java.client.Space;
+
+public class AI {
+	public ArrayList<Space> gameBoard;
+
+	public AI() {
+		initGraph();
+	}
+
+	public void initGraph() {
+
+		gameBoard = new ArrayList<Space>();
+		Space s; // temp space variable
+		// create all the spaces objects
+		for (int i = 0; i < 9; i++) {
+			for (int p = 0; p < 9; p++) {
+				s = new Space(i, p);
+				gameBoard.add(s);
+			}
+		}
+
+		// now we establish the connections
+		for (int i = 0; i < 9; i++) {
+			for (int p = 0; p < 9; p++) {
+				s = gameBoard.get(p + i * 9); // using that temp variable we can
+												// get each node
+
+				s.occupied = false;
+
+				// here we will check if we are adding connections on the top
+				// row.
+				if (i == 0) {
+					// if we are here we will not add a connection to the up
+					// pointer
+					// in the space
+					s.Up = null;
+
+					if (p == 0) {
+						// here we are on the right hand side of the board
+						s.Right = null;
+						s.Left = gameBoard.get(p + (i * 9) + 1); // here we get
+																	// the node
+																	// next to
+																	// the
+																	// current
+																	// one
+						s.Down = gameBoard.get(p + (i * 9) + 9); // get the node
+																	// directly
+																	// below the
+																	// current
+																	// node
+					} else if (p == 8) {
+						// here we are at the left hand side of the board
+						s.Left = null;
+						s.Down = gameBoard.get(p + (i * 9) + 9); // get the node
+																	// directly
+																	// below the
+																	// current
+																	// node
+						s.Right = gameBoard.get(p + (i * 9) - 1); // get the
+																	// node that
+																	// is just
+																	// before
+																	// the
+																	// current
+																	// node
+					} else {
+						// here we are in the middle of the board
+						s.Down = gameBoard.get(p + (i * 9) + 9); // get the node
+																	// directly
+																	// below
+																	// this node
+						s.Right = gameBoard.get(p + (i * 9) - 1); // get the
+																	// node that
+																	// is just
+																	// before
+																	// the
+																	// current
+																	// node
+						s.Left = gameBoard.get(p + (i * 9) + 1); // here we get
+																	// the node
+																	// next to
+																	// the
+																	// current
+																	// one
+					}
+
+				} else if (i == 8) { // here we are looking at the bottom row of
+										// the board
+
+					s.Down = null; // there is no nodes below this node
+
+					if (p == 0) {
+						// here we are on the right hand side of the board
+						s.Right = null;
+						s.Left = gameBoard.get(p + (i * 9) + 1); // here we get
+																	// the node
+																	// next to
+																	// the
+																	// current
+																	// one
+						s.Up = gameBoard.get(p + (i * 9) - 9); // get the node
+																// directly
+																// above the
+																// current node
+					} else if (p == 8) {
+						// here we are at the left hand side of the board
+						s.Left = null;
+						s.Up = gameBoard.get(p + (i * 9) - 9); // get the node
+																// directly
+																// above the
+																// current node
+						s.Right = gameBoard.get(p + (i * 9) - 1); // get the
+																	// node that
+																	// is just
+																	// before
+																	// the
+																	// current
+																	// node
+					} else {
+						// here we are in the middle of the board
+						s.Up = gameBoard.get(p + (i * 9) - 9); // get the node
+																// directly
+																// above this
+																// node
+						s.Right = gameBoard.get(p + (i * 9) - 1); // get the
+																	// node that
+																	// is just
+																	// before
+																	// the
+																	// current
+																	// node
+						s.Left = gameBoard.get(p + (i * 9) + 1); // here we get
+																	// the node
+																	// next to
+																	// the
+																	// current
+																	// one
+					}
+				} else { // here we are looking at the middle of the board
+					if (p == 0) {
+						// here we are on the right hand side of the board
+						s.Right = null;
+						s.Left = gameBoard.get(p + (i * 9) + 1); // here we get
+																	// the node
+																	// next to
+																	// the
+																	// current
+																	// one
+						s.Up = gameBoard.get(p + (i * 9) - 9); // get the node
+																// directly
+																// above the
+																// current node
+						s.Down = gameBoard.get(p + (i * 9) + 9); // get the node
+																	// directly
+																	// below
+																	// this node
+					} else if (p == 8) {
+						// here we are at the left hand side of the board
+						s.Left = null;
+						s.Up = gameBoard.get(p + (i * 9) - 9); // get the node
+																// directly
+																// above the
+																// current node
+						s.Right = gameBoard.get(p + (i * 9) - 1); // get the
+																	// node that
+																	// is just
+																	// before
+																	// the
+																	// current
+																	// node
+						s.Down = gameBoard.get(p + (i * 9) + 9); // get the node
+																	// directly
+																	// below
+																	// this node
+					} else {
+						// here we are in the middle of the board
+						s.Up = gameBoard.get(p + (i * 9) - 9); // get the node
+																// directly
+																// above this
+																// node
+						s.Right = gameBoard.get(p + (i * 9) - 1); // get the
+																	// node that
+																	// is just
+																	// before
+																	// the
+																	// current
+																	// node
+						s.Left = gameBoard.get(p + (i * 9) + 1); // here we get
+																	// the node
+																	// next to
+																	// the
+																	// current
+																	// one
+						s.Down = gameBoard.get(p + (i * 9) + 9); // get the node
+																	// directly
+																	// below
+																	// this node
+					}
+				}
+
+			}
+		}
+	}
+}

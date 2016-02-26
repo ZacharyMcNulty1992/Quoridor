0387057af6889d0ba5ccc82a4a087294adb7a59e
Derek Waldron <waldrodt195@potsdam.edu>
2016-02-26 12:19:14 -0500

    updated GUI to a clickable grid. prints coordinate of the square clicked, to the terminal

--- src/main/java/client/gui/Main.java
+++ src/main/java/client/gui/Main.java
@@ -1,21 +1,78 @@
 package main.java.client.gui;
 
-import javafx.application.*;
-import javafx.collections.*;
-import javafx.event.*;
-import javafx.geometry.*;
 import javafx.scene.*;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
+import javafx.application.*;
+import javafx.event.*;
+import javafx.stage.*;
+import javafx.geometry.*;
+import javafx.collections.*;
 import javafx.scene.text.*;
 import javafx.scene.shape.*;
 import javafx.scene.paint.*;
-import javafx.stage.*;
+import javafx.scene.input.*;
 
-public class Main extends Application{ 
+public class Main extends Application{
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
     @Override
+    public void start(final Stage primaryStage) {
+
+        Pane root = new Pane();
+
+        GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.setPadding(new Insets(25, 25, 25, 25));
+        gridPane.setGridLinesVisible(false);
+
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+                Rectangle r = new Rectangle(50, 50, Color.GREEN);
+                gridPane.add(r, i, j);
+                r.setMouseTransparent(false);
+                GridPane.setRowIndex(r, i);
+                GridPane.setColumnIndex(r, j);
+            }
+        }
+
+        root.getChildren().add(gridPane);
+
+        Scene scene = new Scene(root, 600, 600, Color.WHITE);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+
+        gridPane.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+
+                for( Node node: gridPane.getChildren()) {
+
+                    if( node.getBoundsInParent().contains(e.getSceneX(),  e.getSceneY())) {
+                        System.out.println(GridPane.getRowIndex(node) + "," + GridPane.getColumnIndex(node));
+                    }
+                }
+            }
+        });
+
+    }
+}
+   /* @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Quoridor");
+
+        Group root = new Group();
+        Scene theScene = new Scene(root);
+        primaryStage.setScene(theScene);
+
+        theScene.getStylesheets().add
+            (Quoridor.class.getResource("Quoridor.css").toExternalForm());
 
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
@@ -23,46 +80,44 @@ public class Main extends Application{
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
 
-        /*Text scenetitle = new Text("Quoridor");
-        scenetitle.setId("Quoridor-text");
-        grid.add(scenetitle, 0, 0, 2, 1);
+        root.getChildren().add(grid);
 
-        /*GridPane buttonGrid = new GridPane();
-        buttonGrid.setAlignment(Pos.CENTER);
-        buttonGrid.setHgap(10);
-        buttonGrid.setVgap(10);
-        buttonGrid.setPadding(new Insets(25, 25, 25, 25));*/
+        GridPane vGrid = new GridPane();
+        vGrid.setAlignment(Pos.CENTER);
+        vGrid.setHgap(50);
+        vGrid.setVgap(10);
+        vGrid.setPadding(new Insets(25, 25, 25, 25));
 
-        /*Button abtn = new Button("2 Player");
-        HBox hbBtn = new HBox(10);
-        hbBtn.setAlignment(Pos.BOTTOM_LEFT);
-        hbBtn.getChildren().add(abtn);
-        grid.add(hbBtn, 0, 1);
+        root.getChildren().add(vGrid);
 
-        Button bbtn = new Button("4 Player");
-        HBox bBtn = new HBox(10);
-        bBtn.setAlignment(Pos.BOTTOM_RIGHT);
-        bBtn.getChildren().add(bbtn);
-        grid.add(bBtn, 1, 1);*/
+        GridPane hGrid = new GridPane();
+        hGrid.setAlignment(Pos.CENTER);
+        hGrid.setHgap(10);
+        hGrid.setVgap(50);
+        hGrid.setPadding(new Insets(25, 25, 25, 25));
+
+        root.getChildren().add(hGrid);
 
         grid.setGridLinesVisible(false);
 
         for(int i = 0; i < 9; i++) {
             for(int j = 0; j < 9; j++) {
-                grid.add(new Rectangle(50, 50, Color.WHITE), i, j);
+                grid.add(new Rectangle(50, 50, Color.GREEN), i, j);
                 grid.add(new Text(i +", " + j), i, j);
             }
         }
 
-        Scene scene = new Scene(grid, 800, 800);
-        //Scene btnScene = new Scene(buttonGrid, 300, 300);
+        for(int i = 0; i < 8; i++) {
+            for(int j = 0; j < 9; j++) {
+                vGrid.add(new Rectangle(10, 50, Color.RED), i+ 1, j);
+            }
+        } 
 
-        primaryStage.setScene(scene);
-        //primaryStage.setScene(btnScene);
-
-        scene.getStylesheets().add
-            (Main.class.getResource("/Main.css").toExternalForm());
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 8; j++) {
+                hGrid.add(new Rectangle(50, 10, Color.BLUE), i, j + 1);
+            }
+        }
 
         primaryStage.show();
-    }
-}
+    }*/

7b048455793fdcdefbe51e3f8354ba56d15a66d5
Michael Cataldo <cataldmj194@potsdam.edu>
2016-03-20 20:53:22 -0400

    Moved error checking code out of main into separate method for readability purposes. Also tidied up some code for the same reason.

--- src/main/java/client/CMT.java
+++ src/main/java/client/CMT.java
@@ -14,6 +14,12 @@ public class CMT {
 	                                 new ArrayList<ClientThread>();
 
     public static void main(String[] args) throws Exception{
+
+	playerNumber = 0;
+        playerCount = args.length;
+        Socket clientSocket;
+	Scanner keyboard = new Scanner(System.in);
+
         try{
             InetAddress addr;
             addr = InetAddress.getLocalHost();
@@ -21,29 +27,10 @@ public class CMT {
         }catch(UnknownHostException ex){
             System.out.println("Hostname can not be resolved");
         }
-
-        playerNumber = 0;
-        int portNumber = 0;
-        playerCount = args.length;
-        Socket clientSocket;
     
         for(int i = 0; i < args.length; i++){
             String [] temp = args[i].split(":");
-            try{
-                portNumber = Integer.parseInt(temp[1]);
-                hostname = temp[0];
-                if(portNumber < 1024 || portNumber > 65535){
-                    System.out.println();
-                    throw new IndexOutOfBoundsException("Port number must be " +
-                                             "between 1024 and 65535 inclusive");
-                }
-            }catch(NumberFormatException e){
-                System.out.println("\nPort number must be an Integer");
-                System.out.println("You entered: " + temp[0]);
-                System.out.println("Exiting...");
-                System.exit(0);
-            }
-            clientSocket = new Socket(hostname,portNumber);
+	    clientSocket = errorCheck(temp);
             ClientThread client = new ClientThread(++playerNumber,clientSocket);
             threadList.add(client);
             client.start();
@@ -58,7 +45,6 @@ public class CMT {
 	    c.write("GAME " + playerCount + " " + playerNames);
         }
 
-        Scanner keyboard = new Scanner(System.in);
         String line = keyboard.nextLine();
         //IDEA FOR LATER: ADD MYOUSHU, TESUJI(SERVERSIDE) AND ATARI METHODS.
         //                CALL EACH METHOD PER THREAD IN LIST                
@@ -68,6 +54,31 @@ public class CMT {
             line = keyboard.nextLine();
         }
     }
+
+    public static Socket errorCheck(String [] temp){
+	Socket ec = new Socket();
+	int portNumber = 0;
+	try{
+            portNumber = Integer.parseInt(temp[1]);
+            hostname = temp[0];
+            if(portNumber < 1024 || portNumber > 65535){
+               System.out.println();
+               throw new IndexOutOfBoundsException("Port number must be " +
+                                         "between 1024 and 65535 inclusive");
+            }
+	    ec = new Socket(hostname,portNumber);
+        }catch(NumberFormatException e){
+            System.out.println("\nPort number must be an Integer");
+            System.out.println("You entered: " + temp[0]);
+            System.out.println("Exiting...");
+            System.exit(0);
+        }catch(IOException e){
+	    System.out.println(e);
+	    System.exit(0);
+	}
+	return ec;	
+    } 
+
 }  
 
   class ClientThread extends Thread{

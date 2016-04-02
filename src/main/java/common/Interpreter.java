87e5c322279412b8f096c32fbc6e372f31de6d1f
Brandon Williams <bkwilliams92@yahoo.com>
2016-04-02 18:01:32 -0400

    For whatever reason the Interpreter java file was not added in my last commit.

--- src/main/java/common/Interpreter.java
+++ src/main/java/common/Interpreter.java
@@ -2,131 +2,126 @@
 
 package common;
 
-import java.awt.Point;
-import java.util.*;
-import java.io.*;
-
-
 /**
  * @author Jade
  * @edited Brandon
- * @date 3/30/16
+ * @date 3/31/16
  */
 public class Interpreter {
 
-    /**
-     * Parses and validates the message coming in from
-     * the server.
-     * 
-     * @param input The message recieved from the server.
-     */
-    public static String[] parseString(String input){
-	
-	String[] splitInput = input.split(" ");
-	
-	switch(splitInput[0]) {
-	    
-	case "TESUJI":
-	    return validateTesuji(splitInput);
-	    
-	default:
-	    //System.out.println("Bad Op Code");
-	    return new String[] {"GOTE"};
-	}
-    } 
-    
-    /**
-     * Checks if proper arguments are given to Tesuji op code
-     * 
-     * @param args The arguments passed to Tesuji.
-     */
-    private static String[] validateTesuji(String[] args) {
-	
-	String moveArg = "";
+  /**
+   * Parses and validates the message coming in from
+   * the server.
+   * 
+   * @param input The message recieved from the client or server.
+   */
+  public static String[] parseString(String input){
 
-	//If it is a pawn move
-	if(args[1].startsWith("(")) {
-	    
-	    //If there are no spaces in the <move> string
-	    if(args.length == 2){
-		
-		//Replace the parenthesis with empty string 
-		moveArg = args[1].replace("(", "")
-		    .replace(")",""); 
-		
-	    }// end if args.length == 2
-	    
-	    else if(args.length == 3) {
-		
-		//Remove parenthesis
-		moveArg = args[1].replace("(", "") + 
-		    args[2].replace(")", "");
-	    }
-	}
-	    
-	else if(args[1].startsWith("[")) {
+    String[] splitInput = input.split(" ");
 
-	    //System.out.println("Wall move seen");
-	    
-	    if(args.length == 2){
+    switch(splitInput[0]) {
 
-		//System.out.println("Wall move with no spaces seen");
-		
-		//Replace the parenthesis and brackets with empty string 
-		moveArg = args[1].replace("[(", "")
-		    .replace(")",""). replace("]","");
+    case "TESUJI":
+      return validateTesuji(splitInput);
 
-		//System.out.println(moveArg);
-	    }
+    default:
+      //System.out.println("Bad Op Code");
+      return new String[] {"GOTE"};
+    }
+  } 
 
-	    else if( args.length == 4 ) {
+  /**
+   * Checks if proper arguments are given to Tesuji op code
+   * 
+   * @param args The arguments passed to Tesuji.
+   */
+  private static String[] validateTesuji(String[] args) {
 
-		// Replace parenthesis and brackets with empty string
-		// and concantenate elements 
-		moveArg = args[1].replace("[(", "") +
-		    args[2].replace(")", "") + 
-		    args[3].replace("]", "");
-	    }
+    String moveArg = "";
 
-	}// end if args[1] startWith "["
+    //If it is a pawn move
+    if(args[1].startsWith("(")) {
 
-	return validateMoveString(moveArg);
+      //If there are no spaces in the <move> string
+      if(args.length == 2){
+
+        //Replace the parenthesis with empty string 
+        moveArg = args[1].replace("(", "")
+            .replace(")",""); 
+
+      }// end if args.length == 2
+
+      else if(args.length == 3) {
+
+        //Remove parenthesis
+        moveArg = args[1].replace("(", "") + 
+            args[2].replace(")", "");
+      }
     }
 
-    private static String[] validateMoveString(String moveString) {
+    else if(args[1].startsWith("[")) {
 
-	//Then split on the comma since there are no spaces
-	String[] position = moveString.split(",");
+      //System.out.println("Wall move seen");
 
-	/*	for(String s : position)
+      if(args.length == 2){
+
+        //System.out.println("Wall move with no spaces seen");
+
+        //Replace the parenthesis and brackets with empty string 
+        moveArg = args[1].replace("[(", "")
+            .replace(")",""). replace("]","");
+
+        //System.out.println(moveArg);
+      }
+
+      else if( args.length == 4 ) {
+
+        // Replace parenthesis and brackets with empty string
+        // and concantenate elements 
+        moveArg = args[1].replace("[(", "") +
+            args[2].replace(")", "") + 
+            args[3].replace("]", "");
+      }
+
+    }// end if args[1] startWith "["
+
+    return validateMoveString(moveArg);
+  }
+
+  private static String[] validateMoveString(String moveString) {
+
+    //Then split on the comma since there are no spaces
+    String[] position = moveString.split(",");
+
+    /*	for(String s : position)
 	    System.out.print(s + " ");
 
 	    System.out.println();*/
 
-	try {	
-	    //
-	    if(position.length == 2) {	
-		
-		Integer.parseInt(position[0]);
-		Integer.parseInt(position[1]);
-	    }
-	    
-	    else if(position.length == 3 &&
-		    (position[2].equals("h") || 
-		     position[2].equals("v")) ) {
-		
-		Integer.parseInt(position[0]);
-		Integer.parseInt(position[1]);
-	    }
-	    
-	    else
-		return new String[] {"GOTE"};
+    try {	
+      //
+      if(position.length == 2) {	
 
-	} catch(NumberFormatException ex) {
-	    
-	    return new String[] {"GOTE"};
-	}
-	
-	return position;
+        Integer.parseInt(position[0]);
+        Integer.parseInt(position[1]);
+      }
+
+      else if(position.length == 3 &&
+          (position[2].equals("h") || 
+              position[2].equals("v")) ) {
+
+        Integer.parseInt(position[0]);
+        Integer.parseInt(position[1]);
+      }
+
+      else
+        return new String[] {"GOTE"};
+
+    } catch(NumberFormatException ex) {
+
+      return new String[] {"GOTE"};
     }
+
+    return position;
+  }
 }
